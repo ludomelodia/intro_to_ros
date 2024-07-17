bdb78d17ea48f85025e84d4e23998f1ebc3cbc24
ludomelodia <ludomelodia@gmail.com>
2024-07-17 08:57:34 -0400

    Added static and differential pressure

--- intro_to_ros/bluerov2_sensors.py
+++ intro_to_ros/bluerov2_sensors.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import BatteryState
 from sensor_msgs.msg import Imu
+from sensor_msgs.msg import FluidPressure
 from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
 
 import numpy as np
@@ -40,17 +41,53 @@ class problem1(Node):
         self.Imu = Imu()
         self.get_logger().info("starting IMU data node")
 
+
+        self.diffpressure = self.create_subscription(
+            FluidPressure,
+            "/mavros/imu/diff_pressure",
+            self.callbackDiffPressure,
+            qos_profile
+        )
+        self.diffpressure
+        self.FluidPressure = FluidPressure()
+        self.get_logger().info("starting differential pressure node")
+
+
+        self.staticpressure = self.create_subscription(
+            FluidPressure,
+            "/mavros/imu/static_pressure",
+            self.callbackStaticPressure,
+            qos_profile
+        )
+        self.staticpressure
+        self.FluidPressure = FluidPressure()
+        self.get_logger().info("starting static pressure node")
+
         self.battery_timer = self.create_timer(5.0, self.voltagecheck)
+        """creates function to check voltage every 5 seconds"""
 
     def callbackBattery(self, msg):
+        """Callback function for the battery. Prints battery voltage"""
         self.BatteryState = msg
-        self.get_logger().info(f"\nBattery Voltage {self.BatteryState.current} {self.BatteryState.voltage}")
+        self.get_logger().info(f"\nBattery Voltage {self.BatteryState.voltage}")
 
     def callbackImu(self, msg):
+        """callback function for the battery. Prints IMU header data"""
         self.Imu = msg
-        #self.get_logger().info(f"\nImu data {self.Imu.header}")
+        self.get_logger().info(f"\nImu data {self.Imu.linear_acceleration}")
+
+    def callbackDiffPressure(self,msg):
+        """callback function for differential pressure"""
+        self.Imu = msg
+        self.get_logger().info(f"\nImu data {self.FluidPressure.fluid_pressure}")
+
+    def callbackStaticPressure(self,msg):
+        """callback function for static pressure"""
+        self.Imu = msg
+        self.get_logger().info(f"\nImu data {self.FluidPressure.fluid_pressure}")
 
     def voltagecheck(self):
+        """checks if battery voltage is safe"""
         if self.BatteryState.voltage < 12:
             self.get_logger().info("battery voltage is too low")
         else:
